e08795f57d0313248d64fb7ba7183f70ef4b7576
Vinícius Ferreira <40644154+Vferreira91@users.noreply.github.com>
2023-09-11 15:46:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-arquivo = 'C:\\Users\\Casa\\Desktop\\pluv.csv'
+arquivo = 'C:\\Users\\03324795\\Desktop\\pluvi.csv'
 
 df = pd.read_csv(arquivo)
 
@@ -31,7 +31,40 @@ df = df.drop(['hora', 'minuto', 'segundo'], axis=1)
 #Remover a coluna 'acumulado_chuva_15_min'
 df = df.drop(['acumulado_chuva_15_min'], axis=1)
 
+
+#Criar um dataframe menor, com apenas os dados da estacao1
+df_estacao1 = df[df['id_estacao'] == 1]
+#Dropar as colunas 1h, 4h e 24h
+df_estacao1 = df_estacao1.drop(['acumulado_chuva_1_h','acumulado_chuva_4_h','acumulado_chuva_24_h'],axis=1)
+
+#Dividir a coluna 'horario' em horas, minutos e segundos:
+df_estacao1[['hora','minuto','segundo']] = df_estacao1['horario'].str.split(':', expand=True)
+#Preencher com 0 onde estiver presente valores NaN
+df_estacao1[['hora','minuto','segundo']] = df_estacao1[['hora','minuto','segundo']].fillna(0)
+# Converter os valores de 'hora', 'minuto' e 'segundo' em inteiros
+df_estacao1[['hora', 'minuto', 'segundo']] = df_estacao1[['hora', 'minuto', 'segundo']].astype(int)
+# Criar uma condição de filtro para excluir linhas em que horas, minutos e segundos são diferentes de zero
+filtro = (df_estacao1['hora'] == 0) & (df_estacao1['minuto'] == 0) & (df_estacao1['segundo'] == 0)
+#Aplicar a mascara booleana no df_estacao1
+df_estacao1 = df_estacao1[filtro]
+#dropar as colunas horario, hora, minuto e segundo do df_estacao1
+df_estacao1 = df_estacao1.drop(['horario','hora', 'minuto', 'segundo'], axis=1)
+
+#com isso ficamos apenas com os dados do acumulado de chuvas das ultimas 96 horas
+#da 0hora de cada dia
+
+#alguns dados da coluna estao com NaN, preencher com o dado da celula anterior
+df_estacao1['acumulado_chuva_96_h'].fillna(method='ffill', inplace=True)
+
+# Criando as colunas latitude e longitude
+df_estacao1['latitude'] = 0.0 
+df_estacao1['longitude'] = 0.0
+
+# Preenchendo os valores para 'latitude' e 'longitude' onde 'id_estacao' é igual a 1
+df_estacao1.loc[df_estacao1['id_estacao'] == 1, 'latitude'] = -22.9925
+df_estacao1.loc[df_estacao1['id_estacao'] == 1, 'longitude'] = -43.23306
+
 #Salvando o dataframe tratado como csv
-df.to_csv('C:\\Users\\Casa\\Desktop\\pluv_tratado.csv', index=False)
+df_estacao1.to_csv('C:\\Users\\03324795\\Desktop\\pluvi_e1.csv', index=False)
 
 
