4e75079b6d9133dd345d157eb67e9dd333357bdd
Vinícius Ferreira <40644154+Vferreira91@users.noreply.github.com>
2023-09-12 18:31:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 
-arquivo = 'C:\\Users\\03324795\\Desktop\\pluvi.csv'
+arquivo_casa = 'C:\\Users\\03324795\\Desktop\\pluvi.csv'
+arquivo_trabalho = 'C:\\Users\\03324795\\Desktop\\pluvi.csv'
+arquivo_note_ju = 'C:\\Users\\Juliana\\Desktop\\pluvi.csv'
+localizacao_estacoes = 'C:\\Users\\Juliana\\Desktop\\dados_estacoes.csv'
 
-df = pd.read_csv(arquivo)
+df = pd.read_csv(arquivo_note_ju)
+local_estacoes = pd.read_csv(localizacao_estacoes)
+#Dropando as colunas estacao, cota, x, y, endereco, situacao, data_inicio_operaco
+#data_fim_operacao, data_atualizacao
+local_estacoes = local_estacoes.drop(['estacao', 'cota', 'x','y','endereco',
+                                      'situacao','data_inicio_operacao',
+                                      'data_fim_operacao','data_atualizacao'], axis=1)
 
-print(df.head())
+#dropar as linhas id_estacao == 40, 41, 42 e 43
+condicao = (local_estacoes['id_estacao'] >= 40) & (local_estacoes['id_estacao'] <= 43)
+
+# Use a função drop para excluir as linhas com base na condição
+local_estacoes = local_estacoes.drop(local_estacoes[condicao].index)
+
+print('Realizado 01 de 10 |#---------|')
+#print(df.head())
 #dropar a coluna primary key pois não nos interessa
 df = df.drop('primary_key', axis=1)
-
+print('Realizado 02 de 10 |##--------|')
 #ordenando o dataframe por: id_estacao, data_particao e horario 
 df = df.sort_values(by=['id_estacao','data_particao', 'horario'], ascending=[True,True,True])
-
+print('Realizado 03 de 10 |###-------|')
 #Dividir a coluna 'horario' em horas, minutos e segundos:
 df[['hora','minuto','segundo']] = df['horario'].str.split(':', expand=True)
-
+print('Realizado 04 de 10 |####------|')
 #Preencher com 0 onde estiver presente valores NaN
 df[['hora','minuto','segundo']] = df[['hora','minuto','segundo']].fillna(0)
+print('Realizado 05 de 10 |#####-----|')
 # Converter os valores de 'hora', 'minuto' e 'segundo' em inteiros
 df[['hora', 'minuto', 'segundo']] = df[['hora', 'minuto', 'segundo']].astype(int)
-
+print('Realizado 06 de 10 |######----|')
 # Criar uma condição de filtro para excluir linhas em que horas, minutos e segundos são diferentes de zero
 filtro = (df['hora'] == 0) & (df['minuto'] == 0) & (df['segundo'] == 0)
-
+print('Realizado 07 de 10 |#######---|')
 # Aplicar o filtro para excluir as linhas
 df = df[filtro]
 
@@ -52,11 +70,8 @@ df = df.drop(['horario', 'data_particao', 'ano','mes','dia'], axis=1)
 #O dataframe restante é o acumulado de chuvas das ultimas 96h
 # registrado a 00:00:00 do dia 01-01-2015 de cada uma das estacoes
 
-
-#O que fazer a seguir:
-#Criar duas colunas, latitude e longitude e aplicar o valor referente ao
-#id_estacao
-
+#passando os valores de latitude e longitude do df'local_estacoes' para o df 'df'
+df = df.merge(local_estacoes[['id_estacao', 'latitude', 'longitude']], on='id_estacao', how='left')
 
 
 
@@ -98,4 +113,3 @@ df_estacao1.loc[df_estacao1['id_estacao'] == 1, 'longitude'] = -43.23306
 #Salvando o dataframe tratado como csv
 df_estacao1.to_csv('C:\\Users\\03324795\\Desktop\\pluvi_e1.csv', index=False)
 '''
-
